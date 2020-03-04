63d5ad015b3039b03d2b3b290bad5b13186cb9c0
juanixba <61675225+juanixba@users.noreply.github.com>
2020-03-03 23:40:39 -0600

    Activity 02 – Simple AI
    Create a simple program that asks for user data and changes the output based on that data.

---
+++ newfileAI.py
@@ -0,0 +1,10 @@
+#AI
+numero = int(input('por favor, si usted es humano digite un numero positivo, si no lo es, por favor digite un numero negativo'))
+
+if numero>0:	
+print	('gracias, usted es bienvenido')
+
+	elif numero ==0:
+print	('eres diosito')
+
+		else: print	('lo sentimos, esto es exclusivo para humanos')
