e69dadf55e0b5a5a883815f28e95b64fcb5ee2f7
juanixba <61675225+juanixba@users.noreply.github.com>
2020-03-03 22:43:16 -0600

    Activity 01 – Python introduction
    Create a simple program that prints a profile by using variables and formatting strings.

---
+++ newfile.py
@@ -0,0 +1,8 @@
+first_name= 'juan carlos'
+last_name= 'ixba rodriguez'
+age= '22'	
+height= '1.8'
+
+print('name:{},{}'.format(last_name, first_name))
+print('age:{}'.format(age))
+print('height: {}' .format(height))
